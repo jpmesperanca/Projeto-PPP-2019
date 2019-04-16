1a88e90dfc1c59f77174be886bdf42493071fa6e
JoaoMarcelino <j.13.marcelino@gmail.com>
2019-04-16 13:34:56 +0100

    First Real Commit

---
+++ Planeamento de Viagens/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu(){
+    printf(".......................");
+    printf("\n\t -Menu-");
+    printf("\n1 - Login");
+    printf("\n2 - Register");
+    printf("\n.......................");
+}
+
+int main()
+{
+    menu();
+    return 0;
+}
