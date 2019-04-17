71504cab29556ebd793569aae141b47eb93132ac
JoaoMarcelino <j.13.marcelino@gmail.com>
2019-04-17 15:27:55 +0100

    Simple Stuff
    Comecei a tentar fazer um menu, mas o scanf nao gosta de mim
    
    locais.txt criado num formato simpatico de uso cof cof

--- Planeamento de Viagens/main.c
+++ Planeamento de Viagens/main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void login(){printf("login");}
+void regist(){printf("regist");}
+
+
 void menu(){
+    char num='0';
     printf(".......................");
     printf("\n\t -Menu-");
     printf("\n1 - Login");
     printf("\n2 - Register");
     printf("\n.......................");
+    printf("\nEscolha: ");
+    scanf("%c",&num);   /* SCANF IS A PAIN IN THE ASS - THANK YOU */
+    switch (num){
+        case '1':
+            login();break;
+        case '2':
+            regist();break;
+        default:
+            menu();break;
+    }
 }
 
 int main()
@@ -14,3 +29,4 @@ int main()
     menu();
     return 0;
 }
+
